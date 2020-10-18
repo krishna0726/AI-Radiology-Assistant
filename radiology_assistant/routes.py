a18e1ac995e84e597da788f667ec587314c0e6a1
engelln <engelln@go.stockton.edu>
2020-10-18 02:34:50 -0400

    Created home page prototype

--- radiology_assistant/routes.py
+++ radiology_assistant/routes.py
@@ -1,5 +1,14 @@
 from radiology_assistant import app
+from flask import render_template
 
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template("home.html")
+
+@app.route("/search")
+def search():
+    return "Search Results Page"
+
+@app.route("/report")
+def report():
+    return "Report Page"
